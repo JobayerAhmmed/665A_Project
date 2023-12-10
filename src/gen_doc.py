2da51fa37665703c157091de35cbaf84286d1ba1
Jobayer Ahmmed <jobayer.ahmmed@gmail.com>
2023-12-09 19:11:22 -0600

    Added doxygen binary file and Doxyfile from px4

--- src/gen_doc.py
+++ src/gen_doc.py
@@ -7,25 +7,20 @@ import config
 
 
 
-def run_gen_doc():
-    # clone_repo()
-    fix_help()
-    pass
-
-
 def clone_repo():
-    """Clone a repository and put the code in the temp directory."""
-    os.chdir(config.temp_dir)
+    """Clone a repository and put the code in the px4 directory."""
+    print('Cloning PX4-Autopilot repository...')
+    os.chdir(config.root_dir)
 
-    if os.path.exists(config.px4_dir):
-        shutil.rmtree(config.px4_dir)
-        create_dir(config.px4_dir)
-        r = subprocess.run(["git", "clone", config.px4_repo_url, config.px4_dir])
+    if os.path.exists(config.px4_src_dir):
+        shutil.rmtree(config.px4_src_dir)
+        create_dir(config.px4_src_dir)
+        r = subprocess.run(["git", "clone", config.px4_repo_url, config.px4_src_dir])
         if r.returncode != 0:
             print("Failed to clone repo {}".format(config.px4_repo_url))
     else:
-        create_dir(config.px4_dir)
-        r = subprocess.run(["git", "clone", config.px4_repo_url, config.px4_dir])
+        create_dir(config.px4_src_dir)
+        r = subprocess.run(["git", "clone", config.px4_repo_url, config.px4_src_dir])
         if r.returncode != 0:
             print("Failed to clone repo {}".format(config.px4_repo_url))
 
@@ -46,9 +41,10 @@ def fix_help():
     """Remove tripple dash from the start and 
     end of help attributes in Kconfig files.
     """
+    print('Fixing Kconfig help attribute...')
     search_text = '---help---'
     replacement_text = 'help'
-    for root, dirs, files in os.walk(config.px4_dir):
+    for root, dirs, files in os.walk(config.px4_src_dir):
         for filename_o in files:
             if not filename_o.endswith('Kconfig'):
                 continue
@@ -65,4 +61,10 @@ def fix_rsource():
     files in a directory. AutoConfDoc does not work on rsource
     because it uses kextract which does not recognize rsoure.
     """
+    pass
+
+
+if __name__ == "__main__":
+    clone_repo()
+    fix_help()
     pass
